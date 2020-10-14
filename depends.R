18f4632487c09bcb93572ea2e1a1139a24cb3dc7
dleng2242 <56025684+dleng2242@users.noreply.github.com>
2020-10-14 12:15:18 +0100

    Update depends.R

--- depends.R
+++ depends.R
@@ -1,4 +1,4 @@
 # Dependencies for course
 
-install.packages(c("tidyverse", "tidytext", "SnowballC", "ggraph", "igraph", "wordcloud", "topicmodels",
-                   "textdata"))
+install.packages(c("tidyverse", "tidytext", "SnowballC", "ggraph", "igraph", 
+                   "wordcloud", "topicmodels", "textdata", "htmlwidgets"))
